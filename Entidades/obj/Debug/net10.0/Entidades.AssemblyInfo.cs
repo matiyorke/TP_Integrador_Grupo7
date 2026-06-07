65d12d8c03fa67ba04a4c1ab53e06dc3de110f95
matiyorke <cortesgmat@gmail.com>
2026-06-07 15:07:10 -0300

    .

--- Entidades/obj/Debug/net10.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net10.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e2e11b6651baf4fb53cb7739429c5e9088affb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Negocio/obj/Debug/net10.0/Negocio.AssemblyInfo.cs
+++ Negocio/obj/Debug/net10.0/Negocio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b179e685c8e20222e737d366bd5b53ac4f4aa183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e2e11b6651baf4fb53cb7739429c5e9088affb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Negocio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
